e30e649563c5cc101fe63d152de396edcad49517
OnyGooD <molnekkrisz@gmail.com>
2025-03-13 08:51:08 +0100

    Implement plane movement logic with setInterval for animation

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ planeEl.innerText = "🛫";
 planeEl.classList.add("plane");
 gameEl.appendChild(planeEl);
 
-setInterval()
+let x = 0;
+let y = 0;
+
+setInterval(() => {
+    x+= 1;
+    y += Math.floor(Math.random() * 10) -4;
+    planeEl.style.left = x + "px";
+    planeEl.style.bottom = y + "px";
+},100)
