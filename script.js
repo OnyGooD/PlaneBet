3b8a2d27618babf982fa2d5a86d374cf20fdd37a
OnyGooD <molnekkrisz@gmail.com>
2025-03-13 08:56:56 +0100

    Enhance plane movement with vertical oscillation and smooth transitions

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ gameEl.appendChild(planeEl);
 
 let x = 0;
 let y = 0;
+let flyUp = true
 
 setInterval(() => {
-    x+= 1;
-    y += Math.floor(Math.random() * 10) -4;
+    x+= 2;
+    if(flyUp){
+        y += Math.floor(Math.random() * 10);
+        if(y > 100) flyUp = false;
+        }else{
+            y += Math.floor(Math.random() * 10)-5;
+        }
+    
     planeEl.style.left = x + "px";
     planeEl.style.bottom = y + "px";
 },100)
