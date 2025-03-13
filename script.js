d802780b3819370b3a1f889450f6b5575dd7e536
OnyGooD <molnekkrisz@gmail.com>
2025-03-13 08:40:56 +0100

    Add initial HTML, CSS, and JavaScript files for game setup

---
+++ script.js
@@ -0,0 +1,8 @@
+const gameEl = document.getElementById("game");
+
+const planeEl = document.createElement("plane");
+planeEl.innerText = "🛫";
+planeEl.classList.add("plane");
+gameEl.appendChild(planeEl);
+
+setInterval()
