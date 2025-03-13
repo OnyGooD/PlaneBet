a9438790d15781daa640d8e9e7889a76dc64399d
OnyGooD <molnekkrisz@gmail.com>
2025-03-13 13:12:44 +0100

    done

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 const gameEl = document.getElementById("game")
 const multiplierEl = document.getElementById("multiplier")
 const planeEl = document.createElement("span")
-
+const betBtn = document.getElementById("bet")
+const betEl = document.getElementById("bet")
 const moneyEl = document.getElementById("money")
 const sliderEl = document.getElementById("slider")
-let money = 100
+let money = localStorage.getItem("money") || 100
+let bet = 0
+let multiplier = 0.5
 
 function updateMoney() {
     sliderEl.max = money
-    moneyEl.innerText = money + "💰"
+    moneyEl.innerText = Math.round(money) + " 💰"
+    localStorage.setItem("money", money)
 }
-
 updateMoney()
 
+function sliderChange(){
+    bet = sliderEl.value
+    betEl.innerText = bet + " 🎲"
+}
+
+function stop(){
+    money += bet * multiplier
+    bet = 0
+    betBtn.disabled = false
+    updateMoney()
+}
+
 function start() {
+    betBtn.disabled = true
+    money -= bet
+    updateMoney()
+
     gameEl.innerHTML = ""
     planeEl.classList.add("plane")
     planeEl.innerText = "🛫"
     gameEl.appendChild(planeEl)
     gameEl.appendChild(multiplierEl)
 
-    let multiplier = 0.5
+    multiplier = 0.5
     let x = 0
     let y = 0
     let flyUp = true
@@ -37,16 +56,17 @@ function start() {
             y = 0
             planeEl.style.bottom = y + "px"
             planeEl.innerText = "💥"
+            betBtn.disabled = false
             clearInterval(intervalID)
         }
 
         if (x < 450) x += 5
 
         if (flyUp) {
-            y += (Math.floor(Math.random() * 10)) * 3
+            y += (Math.floor(Math.random() * 10)) * 6
             if (y > 100) flyUp = false
         } else {
-            y += (Math.floor(Math.random() * 10) - 5) * 3
+            y += (Math.floor(Math.random() * 10) - 5) * 6
         }
 
         planeEl.style.left = x + "px"
